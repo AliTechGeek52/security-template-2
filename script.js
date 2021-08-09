5453514015f7dc19a7fdfe45eda506c7272a9d78
AliTechGeek52 <mohammadaliraza472@gmail.com>
2021-08-10 03:27:15 +0500

    Files updated

--- script.js
+++ script.js
@@ -47,3 +47,51 @@ setInterval(function(){
         count = 1;
     }
 }, 5000);
+
+
+
+let map;
+
+  function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+          center: { lat: 51.509865, lng: -0.118092 },
+          zoom: 12,
+          mapId: 'a674b08729108013'
+      });
+      //24.899592, 67.024611
+      const image =
+          "mappng2.png";
+      const beachMarker = new google.maps.Marker({
+          position: { lat: 24.899592, lng: 67.024611 },
+          map,
+          icon: image,
+      });
+  }
+
+
+  jQuery(document).ready(function($){
+  
+    window.onload = function (){
+      $(".bts-popup").delay(1000).addClass('is-visible');
+      }
+    
+      //open popup
+      $('.bts-popup-trigger').on('click', function(event){
+          event.preventDefault();
+          $('.bts-popup').addClass('is-visible');
+      });
+      
+      //close popup
+      $('.bts-popup').on('click', function(event){
+          if( $(event.target).is('.bts-popup-close') || $(event.target).is('.bts-popup') ) {
+              event.preventDefault();
+              $(this).removeClass('is-visible');
+          }
+      });
+      //close popup when clicking the esc keyboard button
+      $(document).keyup(function(event){
+          if(event.which=='27'){
+              $('.bts-popup').removeClass('is-visible');
+          }
+      });
+  });
